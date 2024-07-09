8fad33b4bab64e2bed16b0381713964f8e94f83e
Matej Bačo <matejbaco2000@gmail.com>
2024-07-09 14:27:48 +0200

    Appwrite CLI setup

---
+++ functions/sendMails/go.mod
@@ -0,0 +1,15 @@
+module openruntimes/handler
+
+go 1.22.5
+
+require github.com/open-runtimes/types-for-go/v4 v4.0.1
+
+require github.com/appwrite/sdk-for-go v0.0.1-rc.1
+
+require (
+	github.com/Boostport/mjml-go v0.14.6 // indirect
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/tetratelabs/wazero v1.6.0 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+)
---
+++ functions/sendMails/main.go
@@ -0,0 +1,81 @@
+package handler
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"os"
+
+	"github.com/Boostport/mjml-go"
+	"github.com/appwrite/sdk-for-go/client"
+	"github.com/appwrite/sdk-for-go/id"
+	"github.com/appwrite/sdk-for-go/messaging"
+	"github.com/open-runtimes/types-for-go/v4"
+)
+
+func Main(Context *types.Context) types.ResponseOutput {
+	if Context.Req.Path != "/send" || Context.Req.Method != "POST" {
+		return Context.Res.Text("Not Found", 404, nil)
+	}
+
+	Context.Log(Context.Req.Headers["x-appwrite-key"])
+	Context.Log(os.Getenv("APPWRITE_FUNCTION_API_ENDPOINT"))
+	Context.Log(os.Getenv("APPWRITE_FUNCTION_PROJECT_ID"))
+
+	appwriteClient := client.NewClient()
+	appwriteClient.SetEndpoint(os.Getenv("APPWRITE_FUNCTION_API_ENDPOINT"))
+	appwriteClient.SetProject(os.Getenv("APPWRITE_FUNCTION_PROJECT_ID"))
+	appwriteClient.SetKey(Context.Req.Headers["x-appwrite-key"])
+
+	appwriteMessaging := messaging.NewMessaging(appwriteClient)
+
+	subject := "MJML E-mail"
+	input := `
+		<mjml>
+			<mj-body>
+				<mj-section>
+					<mj-column>
+						<mj-divider border-color="#F45E43"></mj-divider>
+						<mj-text font-size="20px" color="#F45E43" font-family="helvetica">
+							Hello World
+						</mj-text>
+					</mj-column>
+				</mj-section>
+			</mj-body>
+		</mjml>
+	`
+
+	output, err := mjml.ToHTML(
+		context.Background(),
+		input,
+		mjml.WithValidationLevel(mjml.Strict),
+		mjml.WithBeautify(false),
+		mjml.WithMinify(true),
+		mjml.WithKeepComments(false),
+	)
+
+	var mjmlError mjml.Error
+	if errors.As(err, &mjmlError) {
+		errorAsJson, _ := json.Marshal(mjmlError)
+		Context.Log(string(errorAsJson[:]))
+	}
+
+	message, err := appwriteMessaging.CreateEmail(
+		id.Unique(),
+		subject,
+		output,
+		messaging.WithCreateEmailHtml(true),
+		messaging.WithCreateEmailUsers([]interface{}{
+			Context.Req.Headers["x-appwrite-user-id"],
+		}),
+	)
+
+	if err != nil {
+		Context.Log(err.Error())
+		return Context.Res.Text("Error", 500, nil)
+	}
+
+	Context.Log(message.Id)
+
+	return Context.Res.Text("OK", 200, nil)
+}
